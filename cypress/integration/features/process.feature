b8e86f79232131c544bcd2f3ae4419b347dbf552
Pablo Congote <pablo.congote@MacBook-Pro-de-alejandra.local>
2021-11-08 23:58:25 -0500

    create the d¡first steps validations

---
+++ cypress/integration/features/process.feature
@@ -0,0 +1,15 @@
+Feature: Search in Aliexpress
+
+     As a Customer we want to see if the second ad from the second results page 
+     when searching for "Iphone" on www.aliexpress.com that has at least 1 item to be bought.
+
+     Scenario: Navigation into Aliexprexx.com
+          Given I open "aliexpress.com" page
+          When I Click "Close ads" Button
+          And I type "iphone" in the "search box"
+          And I Click "lens" Button
+          And I Click "option ad" "1"
+          Then I expect have minimun one product in "stock"
+
+     
+
---
+++ cypress/integration/step_definitions/process.js
@@ -0,0 +1,42 @@
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+
+const url = "https://best.aliexpress.com/"
+const number = { 
+    '1': '1',
+    '2': '2'
+}
+const selectors = {
+    "aliexpress.com": url,
+    "Close ads": ".btn-close",
+    "search box": ".search-key-box",
+    "lens": ".search-button",
+    "option ad": "._9tla3",
+    "stock": ".product-quantity-tip > :nth-child(1) > span"
+}
+
+Given ("I open {string} page", (elementName) =>{
+    cy.visit(selectors[elementName])
+})
+
+When ("I Click {string} Button", (elementName) => {
+    cy.get(selectors[elementName]).click()
+})
+
+When ("I type {string} in the {string}", (text, elementName) =>{
+    cy.get(selectors[elementName]).type(text)
+})
+
+When("I Click {string} {string}", (elementName, text) => {
+    cy.get(selectors[elementName]).eq(text).should($div => {
+        expect($div.attr('target'), 'target').to.equal('_blank')
+    $div.attr('target', '_self')
+  }).click()
+
+})
+
+Then("I expect have minimun one product in {string}",(elementName) =>{
+    cy.get(selectors[elementName]).should("contain", "158")
+        
+})
+
+
