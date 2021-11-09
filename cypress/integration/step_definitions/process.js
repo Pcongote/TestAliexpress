1df4fa9a4f19ad621c836e2b1247b15fc1ca6884
Pablo Congote <pablo.congote@MacBook-Pro-de-alejandra.local>
2021-11-09 16:59:59 -0500

    all verifications completed

--- cypress/integration/step_definitions/process.js
+++ cypress/integration/step_definitions/process.js
@@ -1,18 +1,15 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
 const url = "https://best.aliexpress.com/"
-const number = { 
-    '1': '1',
-    '2': '2'
-}
+
 const selectors = {
     "aliexpress.com": url,
     "Close ads": ".btn-close",
     "search box": ".search-key-box",
     "lens": ".search-button",
     "option ad": "._9tla3",
-    "stock": ".addcart-result-title",
-    "add to cart": ".product-action > .addcart-wrap > .next-btn"
+    "stock": ".product-quantity-tip",
+    
 }
 
 Given ("I open {string} page", (elementName) =>{
@@ -32,12 +29,16 @@ When("I Click {string} {string}", (elementName, text) => {
         expect($div.attr('target'), 'target').to.equal('_blank')
     $div.attr('target', '_self')
   }).click()
-
 })
 
 Then("I expect have minimun one product in {string}",(elementName) =>{
-    cy.get(selectors[elementName]).should('contain', '1')
-        
+    cy.get(selectors[elementName]).invoke('text').then((text) => {
+        const totalStock = parseInt(text)
+        cy.get(totalStock).should((totalStock) => {
+                totalStock > 0
+                expect(totalStock).to.not.equal(0)
+        })  
+    })
 })
 
 
